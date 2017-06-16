d7cbc07a3ac16d24469276cc816cd7db1bba1250
kantpredict <kantpredict@gmail.com>
2017-06-16 02:51:35 +0100

    Add files via upload
    Added code for actual motor driving, amended button logic flow

--- _4th_axis/_4th_axis.ino
+++ _4th_axis/_4th_axis.ino
@@ -10,17 +10,21 @@
 #define PIN_B3 6
 #define PIN_SW1 7 //two-position switch
 #define PIN_POT A0 //setting pot
+#define STEP_PIN 2
+#define DIR_PIN 3
+#define ENA_PIN 8
 
-int potValue;
-int oldPot;
-int degVal;
+int potValue, oldPot, degVal;
+int stepping = 8;
+int reduction = 100;
 int rpmVal;
+long degMov;
 bool mode;
 String btnStr;
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
-AccelStepper stepper1(AccelStepper::DRIVER, 2, 3);
-int hertz = 1;
-float micStep = 8;
+AccelStepper stepper1(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
+int hertz = 8000;
+int micStep = 8;
 
 
 void setup(void)
@@ -29,12 +33,14 @@ void setup(void)
   pinMode(PIN_B2, INPUT_PULLUP);
   pinMode(PIN_B3, INPUT_PULLUP);
   pinMode(PIN_SW1, INPUT_PULLUP); // DPDT switch
-
   Timer1.initialize(1000000 / hertz);
   Timer1.attachInterrupt(stepperRun); // stepper to run at 'hertz'
-  stepper1.setMaxSpeed(10000.0);
-  stepper1.setAcceleration(10000.0);
-  stepper1.setEnablePin(4);
+  stepper1.setPinsInverted(false, false, true);
+  stepper1.setMaxSpeed(5000.0);
+  stepper1.setAcceleration(5000.0);
+  stepper1.setEnablePin(ENA_PIN);
+
+  //stepping = stepping * reduction;
   Serial.begin(9600);
   lcd.begin();
   lcd.setCursor(0, 0);
@@ -43,8 +49,14 @@ void setup(void)
 }
 
 void stepperRun() {
-  stepper1.run();
+  if (mode == 0) {
+    stepper1.run();
+  }
+  else {
+    stepper1.runSpeed();
+  }
 }
+
 
 void OnKeyDownBut1(AdvButton* but) {
   btnStr = "";
@@ -72,20 +84,31 @@ void lcdDraw(int but) {
     lcd.println(btnStr);
     switch (but) {
       case 1:
+        stepper1.enableOutputs();
         btnStr = degVal;
         btnStr.concat(" ANTICLOCKWISE");
+        degMov = moveDegrees(-degVal);
+        stepper1.move(degMov);
+        Serial.println(degMov);
         break;
       case 2:
-        btnStr = degVal;
+        stepper1.disableOutputs();
+        Serial.println("STOP");
         btnStr = "STOP";
         break;
       case 3:
+        stepper1.enableOutputs();
         btnStr = degVal;
         btnStr.concat(" CLOCKWISE");
+        degMov = moveDegrees(degVal);
+        stepper1.move(degMov);
+        Serial.println(degMov);
         break;
       default:
         break;
+
     }
+
     lcd.setCursor(0, 1);
     lcd.println(btnStr);
   }
@@ -96,15 +119,21 @@ void lcdDraw(int but) {
 
     switch (but) {
       case 1:
+        stepper1.enableOutputs();
         btnStr = rpmVal;
         btnStr.concat(" ANTICLOCKWISE");
+        moveSpeed(-(rpmVal));
         break;
       case 2:
+        stepper1.disableOutputs();
+        Serial.println("STOP");
         btnStr = "STOP";
         break;
       case 3:
+        stepper1.enableOutputs();
         btnStr = rpmVal;
         btnStr.concat(" CLOCKWISE");
+        moveSpeed(rpmVal);
         break;
       default:
         break;
@@ -113,15 +142,26 @@ void lcdDraw(int but) {
     lcd.println(btnStr);
   }
 }
+long moveDegrees(long d) {
+  d = d * (reduction * stepping);
+  return d;
+}
+void moveSpeed(int s) {
+  s = s * reduction;
+  stepper1.setSpeed(s);
+  Serial.println(s);
+}
+
 AdvButton but1 = AdvButton(PIN_B1,  NULL, OnKeyDownBut1, NULL, btn_Digital);
 AdvButton but2 = AdvButton(PIN_B2,  NULL, OnKeyDownBut2, NULL, btn_Digital);
 AdvButton but3 = AdvButton(PIN_B3,  NULL, OnKeyDownBut3, NULL, btn_Digital);
 
 void loop() {
-
   ButtonManager::instance()->checkButtons();
   potValue = analogRead(PIN_POT);
-
+  if (stepper1.distanceToGo() == 0) {
+    stepper1.disableOutputs();
+  }
   if (digitalRead(PIN_SW1) == LOW) { //STEP MODE
     mode = 0;
     degVal = map(potValue, 0, 1024, 90, 0);
@@ -133,13 +173,12 @@ void loop() {
 
   else { //CONTINUOUS MODE
     mode = 1;
-    rpmVal = map(potValue, 0, 1024, 60, 0);
+    rpmVal = map(potValue, 0, 1024, 50, 0);
     if (rpmVal != oldPot) {
       lcdDraw(0);
       oldPot = rpmVal;
     }
   }
-
 }
 
 
